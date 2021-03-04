4f0b15f2911c8c6c9c702e565c25f15af0518af4
qcc4p <qcc4p@gmail.com>
2021-03-04 15:45:24 -0800

    An initial quantum walk with Hadamard Coin.

--- src/lib/dumpers.py
+++ src/lib/dumpers.py
@@ -159,43 +159,44 @@ def latex(ir) -> str:
   larr = []
   for q in range(ir.nregs):
      larr.append([])
-     for g in range(ir.ngates):
+     for g in range(ir.ngates+1):
        larr[q].append('')
 
   depth = 0
   for op in ir.gates:
     for r in range(ir.nregs):
       larr[r][depth] = '\\qw&'
-      
+
     if op.is_gate():
       parm = ''
       name = op.name
+      name = name.replace('_adj', '^\dagger')
       if op.name == 'h':
         name = 'H'
       if op.name == 'cu1' or op.name == 'u1':
         name = ''
       if op.val is not None:
-        parm = '({})'.format(helper.pi_fractions(op.val, '\pi'))    
+        parm = '({})'.format(helper.pi_fractions(op.val, '\pi'))
       if op.is_single():
         larr[op.idx0][depth] = '\\gate{}{}{}&'.format('{', name + parm, '}')
         depth = depth + 1
       if op.is_ctl():
-        larr[op.idx0][depth] = '\\ctrl{}{}{}&'.format('{', op.idx1 - op.ctl, '}')
+        larr[op.ctl][depth] = '\\ctrl{}{}{}&'.format('{', op.idx1 - op.ctl, '}')
         larr[op.idx1][depth] = '\\gate{}{}{}&'.format('{', name + parm, '}')
         depth = depth + 1
-      
+
       #if op.val is not None:
       #  res += '({})'.format(helper.pi_fractions(op.val))
       #if op.is_single():
       #  res += f' {reg2str(ir, op.idx0)};\n'
       #if op.is_ctl():
       #  res += f' {reg2str(ir, op.ctl)},{reg2str(ir, op.idx1)};\n'
-  
+
   res = "\\begin{qcc}\n"
   for q in range(ir.nregs):
      for d in range(depth):
         res += larr[q][d]
      res += '\\\\ \n'
-       
+
   res += "\\end{qcc}"
   return res
---
+++ src/quantum_walk.py
@@ -0,0 +1,170 @@
+# python3
+"""Example: Simple quantum walk with Hadamard Coin."""
+
+import numpy as np
+from absl import app
+
+from src.lib import circuit
+from src.lib import ops
+from src.lib import helper
+from src.lib import state
+
+
+def incr(qc, idx, nbits, aux, controller=[]):
+  """Increment-by-1 circuit."""
+
+  # See "Efficient Quantum Circuit Implementation of
+  # Quantum Walks" by Douglas, Wang.
+  #
+  #  -X--
+  #  -o--X--
+  #  -o--o--X--
+  #  -o--o--o--X--
+  #  ...
+  for i in range(0, nbits):
+    ctl=controller.copy()
+    for j in range(nbits-1, i, -1):
+      ctl.append(j+idx)
+    qc.multi_control(ctl, i+idx, aux, ops.PauliX(), "multi-1-X")
+
+
+def decr(qc, idx, nbits, aux, controller=[]):
+  """Decrement-by-1 circuit."""
+
+  # See "Efficient Quantum Circuit Implementation of
+  # Quantum Walks" by Douglas, Wang.
+  #
+  # Similar to incr, except controlled-by-0's are being used.
+  #
+  #  -X--
+  #  -0--X--
+  #  -0--0--X--
+  #  -0--0--0--X--
+  #  ...
+  for i in range(0, nbits):
+    ctl=controller.copy()
+    for j in range(nbits-1, i, -1):
+      ctl.append([j+idx])
+    qc.multi_control(ctl, i+idx, aux, ops.PauliX(), "multi-0-X")
+
+
+def experiment_incr():
+  """Run a few incr experiments."""
+
+  qc = circuit.qc('incr')
+  x = qc.reg(4, 0)
+  aux = qc.reg(4)
+
+  for val in range(15):
+    incr(qc, 0, 4, aux)
+
+    maxbits, _ = qc.psi.maxprob()
+    res = helper.bits2val(maxbits[0:4])
+    if val+1 != res:
+      raise AssertionError('Invalid Result')
+
+
+def experiment_decr():
+  """Run a few decr experiments."""
+  qc = circuit.qc('decr')
+  x = qc.reg(4, 15)
+  aux = qc.reg(4)
+
+  for val in range(15, 0, -1):
+    decr(qc, 0, 4, aux)
+
+    maxbits, _ = qc.psi.maxprob()
+    res = helper.bits2val(maxbits[0:4])
+    if val-1 != res:
+      raise AssertionError('Invalid Result')
+
+
+def incr_mod_9(qc, aux):
+  """Increment-by-1 modulo 9 circuit."""
+
+  # We achieve this with help of an ancilla:
+  #
+  #  -X------------ o  X  0
+  #  -o--X--------- 0  |  0
+  #  -o--o--X------ 0  |  0
+  #  -o--o--o--X--- o  X  0
+  #                 |  |  |
+  #  needs an extra ancillary:
+  #                 |  |  |
+  #  ...            X--o--X  -> |0>
+  #
+  for i in range(0, 4):
+    ctl=[]
+    for j in range(4-1, i, -1):
+      ctl.append(j)
+    qc.multi_control(ctl, i, aux, ops.PauliX(), "multi-X")
+
+  qc.multi_control([0, [1], [2], 3], aux[4], aux, ops.PauliX(), "multi-X")
+  qc.cx(aux[4], 0)
+  qc.cx(aux[4], 3)
+  qc.multi_control([[0], [1], [2], [3]], aux[4], aux, ops.PauliX(), "multi-X")
+
+
+def experiment_mod_9():
+  """Run a few incr-mod-9 experiments."""
+
+  qc = circuit.qc('incr')
+  x = qc.reg(4, 0)
+  aux = qc.reg(5)  # extra aux
+
+  for val in range(18):
+    incr_mod_9(qc, aux)
+    maxbits, _ = qc.psi.maxprob()
+    res = helper.bits2val(maxbits[0:4])
+    if ((val+1) % 9) != res:
+      raise AssertionError('Invalid Result')
+
+
+def simple_walk():
+  """Simple quantum walk, allowing initial experiments."""
+
+  nbits = 8
+  qc = circuit.qc('simple_walk')
+  x = qc.reg(nbits, 0b10000000)
+  aux = qc.reg(nbits, 0)
+  coin = qc.reg(1, 0)
+
+  for _ in range(128):
+    # Using a Hadamard coin, others are possible, of course.
+    qc.h(coin[0])
+    incr(qc, 0, nbits, aux, [coin[0]])
+    decr(qc, 0, nbits, aux, [[coin[0]]])
+
+  # Find and print the non-zero amplitudes for all states
+  for bits in helper.bitprod(nbits):
+    idx_bits = bits
+    for i in range(nbits):
+        idx_bits = idx_bits + (0,)
+    idx_bits0 = idx_bits + (0,)
+    idx_bits1 = idx_bits + (1,)
+
+    # Printing bits0 only, this can be changed, of course.
+    if qc.psi.ampl(*idx_bits0) != 0.0:
+      print('{:5.1f} {:5.4f}'.format(float(helper.bits2val(bits)),
+                                     qc.psi.ampl(*idx_bits0).real))
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+
+  print('Increment...')
+  experiment_incr()
+
+  print('Decrement...')
+  experiment_decr()
+
+  print('Increment mod 9...')
+  experiment_mod_9()
+
+  print('Simple Walk...')
+  simple_walk()
+
+
+if __name__ == '__main__':
+  app.run(main)
