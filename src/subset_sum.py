84fe1521bfc50db1dc38be92aca8929939a74c37
qcc4cp <qcc4cp@gmail.com>
2021-04-04 03:52:57 +0000

    Beautify code.

--- src/subset_sum.py
+++ src/subset_sum.py
@@ -24,14 +24,13 @@ from src.lib import helper
 from src.lib import ops
 
 flags.DEFINE_integer('nmax', 15, 'Maximum number')
-flags.DEFINE_integer('nnum',  6, 'Maximum number of set elements [1-nmax]')
+flags.DEFINE_integer('nnum',  6,
+                     'Maximum number of set elements [1-nmax]')
 flags.DEFINE_integer('iterations', 20, 'Number of experiments')
 
 
-# Select numbers are collected in a list.
-#
 def select_numbers(nmax, nnum) -> list:
-  """Select random, unique random number."""
+  """Select nnum random, unique numbers in range 1..nmax."""
 
   while True:
     l = random.sample(range(1, nmax), nnum)
@@ -40,14 +39,14 @@ def select_numbers(nmax, nnum) -> list:
 
 
 def tensor_diag(n:int, num):
-    """Construct a tensor product from diagonal matrices."""
+    """Construct tensor product from diagonal matrices."""
 
     def tensor_product(w1:float, w2:float, diag):
         return [j for i in zip([x * w1 for x in diag],
                                [x * w2 for x in diag]) for j in i]
 
-    diag = [1, 1]
-    for i in range(n):
+    diag = [1, -1] if num == 0 else [1, 1] 
+    for i in range(1, n):
         if i == num:
             diag = tensor_product(i, -i, diag)
         else:
@@ -58,7 +57,7 @@ def tensor_diag(n:int, num):
 def set_to_diagonal_h(l, nmax) -> np.ndarray:
     """Construct diag(H)."""
 
-    h = [0.0] * 2**(nmax+1)
+    h = [0.0] * 2**nmax
     for num in l:
       diag = tensor_diag(nmax, num)
       for i in range(len(diag)):
@@ -67,52 +66,42 @@ def set_to_diagonal_h(l, nmax) -> np.ndarray:
 
 
 def compute_partition(l):
-    """Compute (inefficiently) the paritiona."""
+    """Compute paritions that add up."""
 
     solutions = []
     for bits in helper.bitprod(len(l)):
-       # Collect in/out sets.
        iset = []
        oset = []
        for i in range(len(bits)):
-           iset.append(l[i]) if bits[i] == 0 else oset.append(l[i])
-
-       lsum = rsum = 0
-       for i in iset:
-           lsum = lsum + i
-       for i in oset:
-           rsum = rsum + i
-
-       if lsum == rsum:
+           (iset.append(l[i]) if bits[i] == 0 else
+            oset.append(l[i]))
+       if sum(iset) == sum(oset):
            solutions.append(bits)
     return solutions
 
 
 def dump_solution(bits, l):
-    res = ''
     iset = []
     oset = []
     for i in range(len(bits)):
-        if bits[i] == 0:
-            iset.append(f'{l[i]:2d}')
-        else:
-            oset.append(f'{l[i]:2d}')
-    res += '+'.join(iset)
-    res += ' == '
-    res += '+'.join(oset)
-    return res
+        (iset.append(f'{l[i]:2d}') if bits[0] == 0 else
+         oset.append(f'{l[i]:2d}'))
+    return '+'.join(iset) + ' == ' + '+'.join(oset)
 
 
 def run_experiment():
     """Run an experiment, compute H, match against 0."""
 
-    l = select_numbers(flags.FLAGS.nmax, flags.FLAGS.nnum)
+    nmax = flags.FLAGS.nmax
+    nnum = flags.FLAGS.nnum
+    
+    l = select_numbers(nmax, nnum)
     solutions = compute_partition(l)
 
-    diag = set_to_diagonal_h(l, flags.FLAGS.nmax)
+    diag = set_to_diagonal_h(l, nmax)
 
     non_zero = np.count_nonzero(diag)
-    if non_zero != 2**(flags.FLAGS.nmax+1):
+    if non_zero != 2**nmax:
        print('Solution should exist...', end='')
        if len(solutions):
            print(' Found Solution:', dump_solution(solutions[0], l))
@@ -122,7 +111,6 @@ def run_experiment():
     if len(solutions):
         print('FALSE Negative -- Solution should NOT exist, but does')
         return -1
-
     return 0
 
 
