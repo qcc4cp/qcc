c9c0f19c85f3c217e5647f10474a3359f645a54d
Robert Hundt <qcc4cp@gmail.com>
2021-09-19 19:37:01 -0700

    2-qubit Pauli representation.

--- src/pauli_rep.py
+++ src/pauli_rep.py
@@ -11,10 +11,7 @@ from src.lib import circuit
 from src.lib import ops
 
 
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-
+def single_qubit():
   for i in range(10):
     # First we construct a circuit with just one, very random qubit.
     #
@@ -52,6 +49,79 @@ def main(argv):
 
     print(f'qubit({qc.psi[0]:11.2f}, {qc.psi[1]:11.2f}) = ', end='')
     print(f'{i:11.2f} I + {x:11.2f} X + {y:11.2f} Y + {z:11.2f} Z')
+
+
+def two_qubit():
+  for iter in range(10):
+  
+    # First we construct a circuit with two, very random qubits.
+    #
+    qc = circuit.qc('random qubit')
+    qc.qubit(random.random())
+    qc.qubit(random.random())
+
+    # Potentially entangle them.
+    qc.h(0)
+    qc.cx(0, 1)
+
+    # Additionally rotate around randomly.
+    for i in range(2):
+      qc.rx(i, math.pi * random.random())
+      qc.ry(i, math.pi * random.random())
+      qc.rz(i, math.pi * random.random())
+
+    # Compute density matrix.
+    rho = qc.psi.density()
+
+    # Every rho can be put in the 2-qubit Pauli representation,
+    # which is this Sum over i, j from 0 to 3 inclusive, representing
+    # the four Pauli matrices (including the Identity):
+    #
+    #                 3
+    #    rho = 1/4 * Sum(c_ij * Pauli_i kron Pauli_j)
+    #                i,j=0
+    #
+    # To compute the various factors c_ij, we multiply the Pauli
+    # tensor products with the density matrix and take the trace. This
+    # trace is the computed factor:
+    #
+    paulis = [ops.Identity(), ops.PauliX(), ops.PauliY(), ops.PauliZ()]
+    c = np.zeros((4, 4), dtype=np.complex64)
+    for i in range(4):
+      for j in range(4):
+         tprod = paulis[i] * paulis[j]
+         c[i][j] = np.trace(rho @ tprod)
+
+    # To test whether the two qubits are entangled, the diagonal factors
+    # (without c[0][0]) are added up. If the sum is < 1.0, the qubit
+    # states are still seperable.
+    #
+    diag = np.abs(c[1][1]) + np.abs(c[2][2]) + np.abs(c[3][3])
+    print(f'{iter}: diag: {diag:5.2f} ', end='')
+    if diag > 1.0:
+       print('--> Entangled')
+    else:
+       print('Seperable')
+
+    # Let's verify the result and construct a density matrix
+    # from the Pauli matrices using the computed factors:
+    #
+    new_rho = np.zeros((4, 4), dtype=np.complex64)
+    for i in range(4):
+      for j in range(4):
+         tprod = paulis[i] * paulis[j]
+         new_rho = new_rho + c[i][j] * tprod
+
+    if (not np.allclose(rho, new_rho / 4, atol=1e-5)):
+      raise AssertionError('Invalid Pauli Representation')
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+    
+  single_qubit()
+  two_qubit()
 
 
 if __name__ == '__main__':
