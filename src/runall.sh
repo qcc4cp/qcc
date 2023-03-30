4c93f932b6a7839e338df5e6753d7d6f2b398ebe
Robert Hundt <qcc4cp@gmail.com>
2023-03-30 09:58:07 -0700

    Run examples directly with Python.

--- src/runall.sh
+++ src/runall.sh
@@ -1,18 +1,22 @@
 # Run all .py targets in this directory.
 #
 # Note that this script uses -c opt on the bazel command-line.
-# This can cause problems in some OS'es (MacOS).
+# This may cause problems on some OS'es but it builds the accelerated C++
+# xgates library. Code will run without it, just about 10x+ slower.
 #
-# The option can be removed, things will just run a little
-# slower.
+# The -c opt option can be removed, things will just run a little slower.
 
 bazel build -c opt lib/... || exit 1
 bazel run -c opt lib/circuit_test || exit 1
 
 for algo in `ls -1 *.py | sort`
 do
-  testcase=`echo $algo | sed s@\.py@@g`
-  echo "--- [$testcase] ------------------------"
-  bazel run -c opt $testcase || exit 1
+    echo
+    echo "--- [$testcase] ------------------------"
+    # Bazel would be the default way to run these:
+    #   testcase=`echo $algo | sed s@\.py@@g`
+    #   bazel run -c opt $testcase || exit 1
+    #
+    # But it is also possible, perhaps easier, to run directly via Python:
+    python $testcase.py || exit 1
 done
-
