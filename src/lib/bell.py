9f5d975b45719777842002f4186446531b2d0daa
qcc4cp <qcc4cp@gmail.com>
2024-03-10 07:17:09 -0700

    Use assert.

--- src/lib/bell.py
+++ src/lib/bell.py
@@ -10,9 +10,7 @@ from src.lib import state
 def bell_state(a: int, b: int) -> state.State:
   """Make one of the four bell states with a, b from {0,1}."""
 
-  if a not in [0, 1] or b not in [0, 1]:
-    raise ValueError('Bell state arguments are bits and must be 0 or 1.')
-
+  assert a in [0, 1] and b in [0, 1], 'Bits must be 0 or 1.'
   psi = state.bitstring(a, b)
   psi = ops.Hadamard()(psi)
   return ops.Cnot()(psi)
